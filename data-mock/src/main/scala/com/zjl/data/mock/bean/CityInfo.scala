c3ee8001e0ebaa6fc2561712d2202b5d573a5efd
yisuoyanyv <840834298@qq.com>
2020-10-19 22:30:08 +0800

    创建module，mock产生数据，并发送到kafka

---
+++ data-mock/src/main/scala/com/zjl/data/mock/bean/CityInfo.scala
@@ -0,0 +1,11 @@
+package com.zjl.data.mock.bean
+
+/**
+ * 城市表
+ * @param city_id 城市id
+ * @param city_name 城市名
+ * @param area  城市区域
+ */
+case class CityInfo (city_id:Long,
+                    city_name:String,
+                    area:String)
